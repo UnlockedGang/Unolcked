0d491fd9dd92f20b4500983f2533329bd2a399cd
UnlockedGang <elsech.gil@proton.me>
2024-08-20 14:46:57 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-}
-
-function openwin() {
-    window.open("", "popup", "width=400,height=400");
+    document.getElementById("mySidenav").style.width = "0";
 }
