dfd708a666014319c929fc359826ffb3651137a1
UnlockedGang <elsech.gil@proton.me>
2024-09-11 18:34:39 +0200

    no encript

--- encrypt.js
+++
@@ -1,3 +0,0 @@
-const url = 'https://www.google.com';  // Reemplaza con tu enlace
-const encryptedUrl = btoa(url);  // Encriptar usando Base64
-console.log(encryptedUrl);  // Muestra la URL encriptada en la consola
